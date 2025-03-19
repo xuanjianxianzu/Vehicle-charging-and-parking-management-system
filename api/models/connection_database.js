a876e06ac1edb8a1438a7d72e03f604b69262c5f
xuanjianxianzu <145917798+xuanjianxianzu@users.noreply.github.com>
2025-03-19 18:57:52 +0800

    The node.js project is generated.

---
+++ api/models/connection_database.js
@@ -0,0 +1,14 @@
+//a method used to connection to database 
+const dbModel = require("./db_model");
+const mysql = require('mysql2');
+
+module.exports = {
+    getConnection:()=>{
+        return mysql.createConnection({
+            host:dbModel.host,
+            user:dbModel.user,
+            password:dbModel.password,
+            database:dbModel.datebase
+        });
+    }
+}
---
+++ api/models/db_model.js
@@ -0,0 +1,7 @@
+//Database details module
+module.exports = {
+    host:'localhost',
+    user:'root',
+    password:'Wadwad2020a',
+    datebase:'vehicle_charging_parking'
+};
