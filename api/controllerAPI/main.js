54d965df087c6e834618cfc8903cfa74308937f1
xuanjianxianzu <145917798+xuanjianxianzu@users.noreply.github.com>
2025-03-30 04:11:28 +0800

    Today I modified the code in the registration section to add hash processing to avoid displaying the password in plaintext. The login api is implemented, and the token verification part is completed. The api was tested using postman.

--- api/controllerAPI/main.js
+++ api/controllerAPI/main.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const dbcon = require('../models/connection_database')
-
-
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.post('/register',async (req,res) =>{
     let connection;
@@ -19,8 +19,8 @@ router.post('/register',async (req,res) =>{
             });
         }
 
-
-        const [result] = await connection.query('INSERT INTO users (username, password) VALUES (?, ?)', [username, password]);
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const [result] = await connection.query('INSERT INTO users (username, password) VALUES (?, ?)', [username, hashedPassword]);
         const newUserId = result.insertId;
 
         const [newUserRows] = await connection.query('SELECT id, username, created_at FROM users WHERE id = ?', [newUserId]);
@@ -51,6 +51,72 @@ router.post('/register',async (req,res) =>{
   
 );
 
+
+router.post('/login',async (req,res) =>{
+    let connection;
+    try {
+        connection = await dbcon.getConnection();
+
+        const { username, password } = req.body;
+        console.log(username);
+        const [rows] = await connection.query('SELECT * FROM users WHERE username = ?', [username]);
+        if (rows.length === 0) {
+            return res.status(404).json({
+                code: 404,
+                message: '用户名不存在'
+            });
+        }
+
+        const loginUser = rows[0];
+
+        console.log(password,loginUser.password);
+
+        const isMatch = await bcrypt.compare(password, loginUser.password);
+
+        console.log(isMatch);
+
+        if(!isMatch){
+            return res.status(401).json({
+                code: 401,
+                message: '密码错误'
+            });
+        }
+
+        const token = jwt.sign(
+            { userId: loginUser.id, username: loginUser.username },
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' }
+        );
+
+        return res.status(200).json({
+            code: 200,
+            data: {
+                id: loginUser.id,
+                username: loginUser.username,
+                token: token,
+                expiresIn: 3600
+            },
+            message: '登录成功'
+        });
+    
+    } catch (error) {
+        console.error('登录错误:', error);
+        return res.status(500).json({ code: 500, message: '服务器内部错误' });
+
+    }finally {
+        if (connection) {
+            await connection.end();
+        }
+    }
+  }
+
+  
+);
+
+
+router.get('/index',(req,res)=>{
+    res.send('ok')
+})
 
 
 
--- api/server.js
+++ api/server.js
@@ -1,7 +1,44 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
 const cors = require('cors')
 const main = require('./controllerAPI/main')
+const jwt = require('jsonwebtoken');
+const app = express();
+
+
+app.use('/api',(req,res,next) => {
+    if(req.url == '/register'||req.url == '/login'){
+        return next();
+    }
+
+    const token = String(req.headers.authorization);
+    console.log(token);
+    if(token == 'undefined'&&token == 'null'){
+        res.status(400).send({
+            data:null,
+            meta: {
+                msg:"token无效",
+                status:400
+            }
+        })
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        return res.status(401).json({
+            data: null,
+            meta: {
+                msg: "token无效或已过期",
+                status: 401
+            }
+        });
+    }
+
+
+})
+
 
 
 app.use(express.json()); 
